0c432e2912713987852c0db421b6dd284d321585
GenPark <135203240+alphaparkinc@users.noreply.github.com>
2026-08-25 20:29:56 +0800

    feat: add client.py

---
+++ client.py
@@ -0,0 +1,11 @@
+class AiVettingTechnicalInterviewTalentMatchingClient:
+    def conduct_ai_technical_vetting_and_match(self, candidate_resume_uri='s3://talent/resumes/c_9941_ml_engineer.pdf', job_role_requirements='Senior Distributed Systems & CUDA Engineer'):
+        return {
+            'vetting_session_id': 'mrc_vet_7721',
+            'target_role': job_role_requirements,
+            'live_coding_ai_interview_score_pct': 94.8,
+            'system_design_competency_tier': 'TOP_1_PERCENT',
+            'verified_past_code_contributions_score': 96.0,
+            'recommended_salary_range_usd': '$180,000 - $240,000',
+            'talent_placement_match_status': 'HIRED_DIRECT_CONTRACT'
+        }
